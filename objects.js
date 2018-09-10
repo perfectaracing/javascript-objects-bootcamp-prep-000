f0189477869ca46811af57790e38777c98a65f5e
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-10 21:49:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {
   Cure: 'Charlotte Sometimes',
   Smiths: 'Panic'
 };
+function UpdatePlaylist(playlist, artistName, songTitle)
+{
+  return Object.assign({}, playlist, [artistName]: songTitle)
+}
