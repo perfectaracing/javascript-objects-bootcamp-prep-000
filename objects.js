6c6aefa9aa76e3189a11455354646d7eb3751dc3
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-10 22:19:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = {artistName:"songTitle"};
  
 function UpdatePlaylist(playlist, artistName, songTitle)
+Object.assign({}, { artistName: 'songTitle'})
 {
    return Object.assign({}, playlist,  { artistName:songTitle
-})
+})}
 
