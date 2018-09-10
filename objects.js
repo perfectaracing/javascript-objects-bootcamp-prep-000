8cef3a99fb11760b8d1282ca98ed7ed845f54cb8
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-10 22:07:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,8 +2,6 @@ var playlist = {artistName:"songTitle"};
  
 function UpdatePlaylist(playlist, artistName, songTitle)
 {
-  playlist = artistName[songTitle]
+  playlist = artistName.songTitle
 }
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName];
-}
+
