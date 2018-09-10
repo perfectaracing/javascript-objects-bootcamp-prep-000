88c598bd59de17a8c42cb211e9353ee143536800
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-10 22:29:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,9 @@ var updatePlaylist = function(playlist, artistName, songTitle)
    return Object.assign({}, playlist,  { [artistName]: songTitle
 })}
 
+var removeFromPlaylist = function(playlist, artistName){
+  var playlist = playlist;
+  delete playlist.artistName
+  return playlist
+}
+
