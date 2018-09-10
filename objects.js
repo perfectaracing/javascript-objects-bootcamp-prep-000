2e52c390f7679a003f9d98e761583bcd5136326b
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-10 21:54:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,7 @@
-var playlist = {
+var playlist = {artistName:"song"};
   Dido: 'Life For Rent',
   Cure: 'Charlotte Sometimes',
   Smiths: 'Panic'
-};
 function UpdatePlaylist(playlist, artistName, songTitle)
 {
   playlist[artistName]=songTitle
